c817cf730fe41a214058d951a85c8f86e6965e6c
dany tsan <danytsan23@gmail.Com>
2026-01-06 15:54:59 +0100

    feat: add logging and debug search_papers iterator issue

--- config/sse_config.py
+++ config/sse_config.py
@@ -7,5 +7,6 @@ from dataclasses import dataclass
 @dataclass
 class SseConfigSTClient:
     "Configuration for remote server"
-    backend_url : str = 'https://techpaperai.onrender.com/sse'
+    backend_url : str = 'http://localhost:8001/sse'
+    #backend_url : str = 'https://techpaperai.onrender.com/sse'
     backend_timeout : int = 60
